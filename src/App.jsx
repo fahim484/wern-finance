63f7f31ded69126bc2d3c24d55c409e3453395d5
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-23 12:03:12 +0600

    vision section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Vision from "./components/Vision";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto">
         <div className="space-y-[128px]">
           <Hero />
+          <Vision />
         </div>
       </div>
     </main>
