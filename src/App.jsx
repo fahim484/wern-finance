2572fee411121604b36e21a9539297518d673e5a
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-23 10:30:00 +0600

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
+    <main className="overflow-x-hidden">
       <Header />
-    </>
+      <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto">
+        <div className="space-y-[128px]">
+          <Hero />
+        </div>
+      </div>
+    </main>
   );
 }
 
